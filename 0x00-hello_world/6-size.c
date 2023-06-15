ea1cd429fbfd4b2d3518fc0118821e4c68b2858a
Feyisope Olaiya <adegokefeyi@yahoo.com>
2023-06-15 10:17:29 -0700

    my 3rd program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	printf("Size of a char:\n", sizeof(char));
-	printf("Size of an int:\n:", sizeof(int));
-	printf("Size of a long int:\n", sizeof(long int));
-	printf("Size of a long long int:\n", sizeof(long long int));
-	printf("Size of a float:\n", sizeof(float));
+	printf("Size of a char:", sizeof(char));
+	printf("Size of an int:", sizeof(int));
+	printf("Size of a long int:", sizeof(long int));
+	printf("Size of a long long int:", sizeof(long long int));
+	printf("Size of a float:", sizeof(float));
 	return (0);
 }
