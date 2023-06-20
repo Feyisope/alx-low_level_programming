2603e63a7c6a6f9961b7e84f23df87777a29bd07
Feyisope Olaiya <adegokefeyi@yahoo.com>
2023-06-20 11:10:16 -0700

    push alphabets 6 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10times
+ *
+ * Return: void
+ **/
+void print_alphabet_x10(void)
+{
+	int n;
+	int k;
+
+	for (k = 1; n <= 10; n++)
+	{
+		for (k = 97; k <= 122; k++)
+		{
+			_putchar(k);
+		}
+		_putchar('\n');
+	}
+}
