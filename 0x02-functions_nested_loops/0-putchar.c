b33eff01cd26108a488d65008d580bbeede5e416
Feyisope Olaiya <adegokefeyi@yahoo.com>
2023-06-20 08:55:35 -0700

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,20 +3,19 @@
 /**
  *main -print _putchar
  *
- *Return_putchar("_");: Always (0)Sucess
+ *Return: Always (0)Sucess
  */
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar("\n");
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
 
