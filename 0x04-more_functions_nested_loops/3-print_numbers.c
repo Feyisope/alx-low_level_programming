295f7f6e7d8e27120287ac7083320946d90abb9f
Feyisope Olaiya <adegokefeyi@yahoo.com>
2023-06-22 09:23:51 -0700

    printnumber

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers
+ * from 0 to 9
+ *
+ * Return: return value
+ **/
+void print_numbers(void)
+{
+	int n = 0;
+
+	do {
+		_putchar (n + 48);
+		n++;
+	}
+	while (n >= 0 && n <= 9)
+		_putchar('\n');
+}
