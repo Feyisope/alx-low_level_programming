6b7afd29cc030141b13f13d0c08b38e5822de5a1
Feyisope Olaiya <adegokefeyi@yahoo.com>
2023-06-22 08:48:55 -0700

    lock code

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *@a: first character
+ *@b: second character
+ *Return: return results
+ **/
+int mul(int a, int b)
+{
+	return (a * b);
+}
