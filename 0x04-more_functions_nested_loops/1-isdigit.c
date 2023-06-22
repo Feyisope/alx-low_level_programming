eff6d9e79ca5ef6108ed54111bbd486e29add96b
Feyisope Olaiya <adegokefeyi@yahoo.com>
2023-06-22 08:12:46 -0700

    push code

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - prints 1 -9
+ *
+ * @c: character used
+ * Return: return 1 if digit else return 0;
+ **/
+int _isdigit(int c)
+{
+	if (c >= 47 && c <= 57)
+		return (1);
+	else
+		return (0);
+}
