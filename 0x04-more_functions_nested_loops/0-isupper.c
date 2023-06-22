33a1ba092e46cbbf92e38e942f9cd5516fcb7fbe
Feyisope Olaiya <adegokefeyi@yahoo.com>
2023-06-22 07:43:10 -0700

    monkeys

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - return 1 for uppercase
+ * and 0 otherwise
+ * @c:character used
+ * Return: Return as indicated
+ **/
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	else
+	return (0);
+}
