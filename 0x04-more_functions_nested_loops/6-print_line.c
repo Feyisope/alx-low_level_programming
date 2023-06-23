058c3bc61e561a996d268ded9fd9335f33548dcb
Feyisope Olaiya <adegokefeyi@yahoo.com>
2023-06-23 15:52:28 -0700

    the power of yet

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - draw a straight line in terminal
+ *
+ * @n: character used.
+ * Return: return the value of n
+ **/
+void print_line(int n)
+{
+	int prlin;
+
+	if (n <= 0)
+	_putchar('n');
+	else
+	{
+		for (prlin = 0; prlin < n; prlin++)
+		{
+			_putchar ('-');
+		_putchar ('\n');
+		}
+	}
+
+}
