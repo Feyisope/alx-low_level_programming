920d991cf3d027f3f0f3cbfa1692a957ad7e80c3
Feyisope Olaiya <adegokefeyi@yahoo.com>
2023-06-23 16:30:24 -0700

    okay o

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,14 +11,16 @@ void print_line(int n)
 	int prlin;
 
 	if (n <= 0)
-	_putchar('n');
+	{
+		_putchar('n');
+	}
 	else
 	{
 		for (prlin = 0; prlin < n; prlin++)
 		{
 			_putchar (95);
+		}
 		_putchar ('\n');
 		}
-	}
 
 }
