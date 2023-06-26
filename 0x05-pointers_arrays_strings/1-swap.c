74581686168caf7956541116108093dbf8658876
Feyisope Olaiya <adegokefeyi@yahoo.com>
2023-06-26 07:37:30 -0700

    every work

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h";
+
+/**
+ * swap_int - swaps the value of two integers
+ *
+ * @a @b: parameter passed
+ * Return: return 0
+ **/
+void swap_int(int *a, int *b)
+{
+	int temp = *a'
+	
+	*a = *b;
+	*b = temp;
+}
