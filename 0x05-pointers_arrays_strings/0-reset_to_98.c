96d55a226a873269f8a2b8a32727ac97908ce612
Feyisope Olaiya <adegokefeyi@yahoo.com>
2023-06-26 06:49:06 -0700

    still learning

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer as a parameter and
+ * updates the value to point to 98
+ * @n: character parameter
+ * Return: retuns the value of 0
+ **/
+void reset_to_98(int *n)
+{
+
+	if (n != 0)
+	{
+	*n = 98;
+	}
+
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+The purpose of this project is to learn pointers, arrays and strings
