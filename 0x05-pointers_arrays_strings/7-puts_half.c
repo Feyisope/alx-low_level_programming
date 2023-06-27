2f91c53bb4f870cd1661ada7ba210bc3c8792a0f
Feyisope Olaiya <adegokefeyi@yahoo.com>
2023-06-27 05:02:35 -0700

    tired

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of the string
+ *
+ * @str:parameter used
+ * Return: return the value of str
+ **/
+void puts_half(char *str)
+{
+	int a, j, leng;
+
+	leng = 0;
+
+	for (a = 0; str[a] != '\0'; a++)
+		lengi++;
+
+	j = (leng / 2);
+
+	if ((leng % 2) == 1)
+		j = ((leng + 1) / 2);
+
+	for (a = j; str[a] != '\0'; a++)
+		_putchar(str[a]);
+	_putchar('\n');
+
+}
