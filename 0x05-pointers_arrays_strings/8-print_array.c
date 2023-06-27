6c62984b0d2960ff54e02817be5ad8c7dc9bafef
Feyisope Olaiya <adegokefeyi@yahoo.com>
2023-06-27 05:22:07 -0700

    i want to sleep

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - prints n elements of an array
+ * @a: array name
+ * @n: no of elements in the array to be printed
+ * Return: input of a and b
+ */
+void print_array(int *a, int n)
+{
+	int k;
+
+	for (k = 0; k < (n - 1); k++)
+	{
+		printf("%d, ", a[k]);
+	}
+		if (k == (n - 1))
+		{
+			printf("%d", a[n - 1]);
+		}
+			printf("\n");
+}
