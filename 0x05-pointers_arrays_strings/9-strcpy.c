5846ebb8d45ab0f68fb4171692dcdca75bbcb177
Feyisope Olaiya <adegokefeyi@yahoo.com>
2023-06-27 05:44:57 -0700

    rubbish

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - copy the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int x = 0;
+	int y = 0;
+
+	while (*(src + x) != '\0')
+	{
+		x++;
+	}
+	for ( ; x < y ; y++)
+	{
+		dest[y] = src[y];
+	}
+	dest[x] = '\0';
+	return (dest);
+}
