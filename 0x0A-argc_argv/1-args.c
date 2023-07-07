9819d7e56fc37530d221e1d368251ab323ed7be9
Feyisope Olaiya <adegokefeyi@yahoo.com>
2023-07-07 09:34:12 -0700

    waterver

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
-#include "main.h"		
+#include "main.h"
 
-/**		
+/**
  * main - print the no of arguments
- * @argc: number of arguments		
- * @argv: array of arguments		
- *	
- * Return: Always 0 (Success)		
- */		
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ **/
 int main(int argc, char *argv[])
 {
 	(void) argv;
-		
+
 	printf("%d\n", argc - 1);
 	return (0);
-		
 }
