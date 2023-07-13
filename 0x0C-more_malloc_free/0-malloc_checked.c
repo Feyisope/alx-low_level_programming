5e8371e0553539a49134b6e205dab0a3d3231e44
Feyisope Olaiya <adegokefeyi@yahoo.com>
2023-07-13 06:01:05 -0700

    return allocation using Malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * malloc_checked - allocates memory using Malloc
+  * @b: size to allocate
+  *
+  * Return: Nothing.
+  */
+void *malloc_checked(unsigned int b)
+{
+	void *j;
+
+	j = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
