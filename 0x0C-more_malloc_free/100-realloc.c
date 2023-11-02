a208c8b390c86161de32abd47edd6411ad786f80
Feyisope Olaiya <adegokefeyi@yahoo.com>
2023-11-02 06:56:34 -0700

    I love coding

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,52 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * _realloc - reallocate memory block
+  * @ptr: ...
+  * @old_size: ...
+  * @new_size: ...
+  *
+  * Return: ...
+  */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *newptr;
+	unsigned int i;
+
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == NULL)
+	{
+		newptr = malloc(new_size);
+
+		if (newptr == NULL)
+			return (NULL);
+
+		return (nptr);
+	}
+	else
+	{
+		if (new_size == 0)
+		{
+			free(ptr);
+			return (NULL);
+		}
+	}
+
+	newptr = malloc(new_size);
+
+	if (newptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < old_size && i < new_size; i++)
+	{
+		newptr[i] = ((char *) ptr)[i];
+	}
+
+	free(ptr);
+	return (newptr);
+}
